d0da838172801f7c63723fc7710b75ce946a51b4
Noah Kernis <nkernis@gmail.com>
2020-02-15 16:27:13 -0500

    organizing code

--- soundscape.py
+++ soundscape.py
@@ -3,24 +3,35 @@
 # authors: Noah Kernis & Sue Roh
 
 import pygame
+import random
 import time
-import threading
 
-media = 'media/'
-files = ['rain.wav', 'frogs.wav', 'waterfall.wav']
+mda_dir = 'media/'
+bg_dir = 'background/'
+aml_dir = 'animal/'
+
+# TODO: pick 
+animal_sounds = ['rain.wav', 'frogs.wav', 'waterfall.wav']
+
+# TODO: pick bg sounds
 loop_files = ['frogs.wav']
 
 pygame.mixer.init()
 
-idx = 0
-
-def setInterval(func, time):
-    e = threading.Event()
-    while not e.wait(time):
-        func()
+# channels 
+# pygame.mixer.set_num_channels(10) (default: 8)
+idx_0 = 0
+idx_1 = 1
+idx_2 = 2
+idx_3 = 3
+idx_4 = 4
+idx_5 = 5
+idx_6 = 6
+idx_7 = 7
 
 def play_sound(sound, sec, idx):
     pygame.mixer.Channel(idx).play(sound, -1, sec)
+
 
 for filename in files:
     pygame.mixer.music.load(media + filename)
@@ -29,8 +40,6 @@ for filename in files:
     channel = pygame.mixer.Channel(idx).play(sound, -1)
     sound.set_volume(0.5)
     idx += 1
-
-idx = 0
 
 for filename in loop_files:
     def playsounds():
@@ -41,4 +50,11 @@ for filename in loop_files:
     setInterval(playsounds, 1)
     idx += 1
 
-time.sleep(100000)
+# plays occasional thunder sounds
+duration = var.thunder_sound.get_length()  # duration of thunder in seconds
+while True:  # infinite while-loop
+    # play thunder sound if random condition met
+    if random.randint(0, 80) == 10:
+        channel2.play(var.thunder_sound)
+    # pause while-loop for duration of thunder
+    time.sleep(duration)
