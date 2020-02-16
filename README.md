acf41456f1a21d2cfe85a38c6dae8de65e36a9a8
Noah Kernis <nkernis@gmail.com>
2020-02-16 17:07:36 -0500

    more audio

--- README.md
+++ README.md
@@ -31,7 +31,10 @@
 
 ### Sounds
 
-- http://soundbible.com/
+Funny one? http://soundbible.com/2039-Tree-Fall-Small.html
+
+#### http://soundbible.com/
+
 - *Background*
 	- http://soundbible.com/1818-Rainforest-Ambience.html
 	- http://soundbible.com/2124-45min-April-Rainstorm.html
@@ -39,7 +42,6 @@
 	- http://soundbible.com/1214-Wetlands.html
 	- http://soundbible.com/1516-Waterfall.html
 	- http://soundbible.com/2032-Water.html
-	- http://soundbible.com/2039-Tree-Fall-Small.html
 - *Animals*
 	- http://soundbible.com/2033-Frogs.html#Frogs%20Sound
 	- http://soundbible.com/2180-Meadowlark.html
@@ -48,10 +50,23 @@
 	- http://soundbible.com/1843-Bluejay-Call-2.html
 	- http://soundbible.com/1842-Bluejay-Call.html
 	- http://soundbible.com/1430-Peacock.html
-	- http://soundbible.com/1205-Killdeer.html
 	- http://soundbible.com/1200-American-Woodcock.html
-	- http://soundbible.com/1974-Falcon.html
-	- http://soundbible.com/1850-Dove.html
 - *Not Natural*
 	- http://soundbible.com/1645-Pling.html
 	- http://soundbible.com/2067-Blop.html
+
+#### http://soundbible.com/
+
+- *Background*
+	- http://www.orangefreesounds.com/gentle-rain-sounds/
+	- http://www.orangefreesounds.com/thunder-lightning-with-rain/
+	- http://www.orangefreesounds.com/light-rain-sound-effect/
+- *Animals*
+	- http://www.orangefreesounds.com/blackbird-tweet/
+	- http://www.orangefreesounds.com/eurasian-collared-dove-call/
+	- http://www.orangefreesounds.com/woodpecker-pecking-sound/
+	- http://www.orangefreesounds.com/crow-voice/
+	- http://www.orangefreesounds.com/bobwhite-quail-call/
+	- http://www.orangefreesounds.com/tawny-owl-call/
+	- http://www.orangefreesounds.com/crane-bird-call-sounds/
+	- http://www.orangefreesounds.com/cuckoo-bird-song/
--- ble_forest.py
+++ ble_forest.py
@@ -9,23 +9,37 @@ import time
 
 # -----> Media <-----
 
+# dirs
 mda_dir = 'media/'
 bg_dir = 'background/'
 aml_dir = 'animal/'
 
+# background sounds
+bg_rainforest_ambiance = 'rainforest_ambiance'
+bg_nature_ambiance = 'nature_ambiance'
+
+bg_gentle_rain = 'gentle_rain'
+bg_light_rain = 'light_rain'
+bg_thunder_lightning_rain = 'thunder_lightning_rain'
+
+# animal sounds
 aml_american_woodcock = 'american_woodcock'
 aml_bluejay_0 = 'bluejay_0'
 aml_bluejay_1 = 'bluejay_1'
 aml_frogs = 'frogs'
-aml_killdeer_0 = 'killdeer_0'
-aml_killdeer_1 = 'killdeer_1'
+aml_killdeer = 'killdeer'
 aml_meadowlark = 'meadowlark'
 aml_peacock = 'peacock'
 aml_warbling_vireo = 'warbling_vireo'
 
-
-bg_rainforest_ambiance = 'rainforest_ambiance'
-bg_nature_ambiance = 'nature_ambiance'
+aml_blackbird = 'blackbird'
+aml_bobwhite_quail_call = 'bobwhite_quail_call'
+aml_crane_call = 'crane_call'
+aml_crow = 'crow'
+aml_cuckoo_bird_song = 'cuckoo_bird_song'
+aml_eurasian_collared_dove_call = 'eurasian_collared_dove_call'
+aml_tawny_owl_call = 'tawny_owl_call'
+aml_woodpecker_pecking = 'woodpecker_pecking'
 
 # -----> Init Audio <-----
 
@@ -39,26 +53,14 @@ pygame.init()
 # set number of channels (default: 8)
 pygame.mixer.set_num_channels(500)
 
-# NOTE:
-# attempting to use music api to avoid
-# conflicts with sound effects on channels
-
 # load bg sound
-bg_path = mda_dir + bg_dir + bg_rainforest_ambiance + ".wav"
-# bg_sound = pygame.mixer.Sound(bg_path)
-
-# find_channel(): find and return an inactive Channel.
-# if no inactive channels and the force argument is True,
-# will find the Channel with the longest running Sound and return it.
+bg_path = mda_dir + bg_dir + bg_rainforest_ambiance + '.wav'
+pygame.mixer.music.load(bg_path)
 
 # set init bg sound volume
-# bg_sound.set_volume(bg_sound_volume)
+pygame.mixer.music.set_volume(bg_sound_volume)
 
 # init bg sound - loop forever
-# pygame.mixer.find_channel(True).play(bg_sound, -1)
-
-pygame.mixer.music.load(bg_path)
-pygame.mixer.music.set_volume(bg_sound_volume)
 pygame.mixer.music.play(loops=-1)
 
 # -----> Data Handlers <-----
@@ -82,6 +84,19 @@ def handle_new_data(data):
 
 def play_sound(sound, sec, idx):
     pygame.mixer.Channel(idx).play(sound, -1, sec)
+    # load bg sound
+    # bg_path = mda_dir + bg_dir + bg_rainforest_ambiance + '.wav'
+    # bg_sound = pygame.mixer.Sound(bg_path)
+
+    # find_channel(): find and return an inactive Channel.
+    # if no inactive channels and the force argument is True,
+    # will find the Channel with the longest running Sound and return it.
+
+    # set init bg sound volume
+    # bg_sound.set_volume(bg_sound_volume)
+
+    # init bg sound - loop forever
+    # pygame.mixer.find_channel(True).play(bg_sound, -1)
 
 # -----> State Handlers <----- (may not need)
 
@@ -95,25 +110,38 @@ def play_sound(sound, sec, idx):
 # may want to randomize volume for every time
 # a sound is played. will "feel" more dynamic...?
 
-# GOOD
+# good
+aml_american_woodcock_path = mda_dir + aml_dir + aml_american_woodcock + '.wav'
+aml_frogs_path = mda_dir + aml_dir + aml_frogs + '.wav'
+aml_peacock_path = mda_dir + aml_dir + aml_peacock + '.wav'
 
-aml_american_woodcock_path = mda_dir + aml_dir + aml_american_woodcock + ".wav"
-aml_frogs_path = mda_dir + aml_dir + aml_frogs + ".wav"
 # a lot in hear, needs edit
-aml_meadowlark_path = mda_dir + aml_dir + aml_meadowlark + ".wav"
-aml_peacock_path = mda_dir + aml_dir + aml_peacock + ".wav"
+aml_meadowlark_path = mda_dir + aml_dir + aml_meadowlark + '.wav'
+
 # long
-aml_warbling_vireo_path = mda_dir + aml_dir + aml_warbling_vireo + ".wav"
+aml_warbling_vireo_path = mda_dir + aml_dir + aml_warbling_vireo + '.wav'
 
-# OK (potentially annoying)
+# potentially annoying...
+aml_bluejay_0_path = mda_dir + aml_dir + aml_bluejay_0 + '.wav'
+aml_bluejay_1_path = mda_dir + aml_dir + aml_bluejay_1 + '.wav'
+aml_killdeer_path = mda_dir + aml_dir + aml_killdeer + '.wav'
 
-aml_bluejay_0_path = mda_dir + aml_dir + aml_bluejay_0 + ".wav"
-aml_bluejay_1_path = mda_dir + aml_dir + aml_bluejay_1 + ".wav"
-aml_killdeer_0_path = mda_dir + aml_dir + aml_killdeer_0 + ".wav"
 
-# BAD
+# not reviewed yet
+bg_gentle_rain_path = mda_dir + bg_dir + bg_gentle_rain + '.wav'
+bg_light_rain_path = mda_dir + bg_dir + bg_light_rain + '.wav'
+bg_thunder_lightning_rain_path = mda_dir + \
+    bg_dir + bg_thunder_lightning_rain + '.wav'
 
-aml_killdeer_1_path = mda_dir + aml_dir + aml_killdeer_1 + ".wav"
+aml_blackbird_path = mda_dir + aml_dir + '.wav'
+aml_bobwhite_quail_call_path = mda_dir + aml_dir + '.wav'
+aml_crane_call_path = mda_dir + aml_dir + '.wav'
+aml_crow_path = mda_dir + aml_dir + '.wav'
+aml_cuckoo_bird_song_path = mda_dir + aml_dir + '.wav'
+aml_eurasian_collared_dove_call_path = mda_dir + aml_dir + '.wav'
+aml_tawny_owl_call_path = mda_dir + aml_dir + '.wav'
+aml_woodpecker_pecking_path = mda_dir + aml_dir + '.wav'
+
 
 TEST_ANIMAL_sound = pygame.mixer.Sound(aml_warbling_vireo_path)
 TEST_ANIMAL_sound.set_volume(0.4)
