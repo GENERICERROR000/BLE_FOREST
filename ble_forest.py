b4fde796dc698934496c6535f05448d965f09c0b
Noah Kernis <nkernis@gmail.com>
2020-02-24 11:06:14 -0500

    new comporessed audio

--- ble_forest.py
+++ ble_forest.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# created: 20/14/2020
+# created: 02/14/2020
 # authors: Noah Kernis & Sue Roh
 
 
@@ -77,14 +77,11 @@ not_natural_blop_path = not_natural_media_path + not_natural_blop + '.wav'
 
 # init pygame
 pygame.mixer.pre_init()
-# pygame.mixer.init()
-
-pygame.mixer.init(44100, -32, 200, 1024)
-
+pygame.mixer.init(48000, -16, 1, 1024)
 pygame.init()
 
 # set number of channels (default: 8)
-# pygame.mixer.set_num_channels(500)
+pygame.mixer.set_num_channels(200)
 
 device_status_sound_map = {
     'idle':  pygame.mixer.Sound(aml_eurasian_collared_dove_call_path),
