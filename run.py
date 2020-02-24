f7fb54c6ce6960e83f61b918a83ef56660c8194e
Noah Kernis <nkernis@gmail.com>
2020-02-24 01:22:46 -0500

    pygame init settings

--- run.py
+++ run.py
@@ -99,7 +99,7 @@ not_natural_blop_path = not_natural_media_path + not_natural_blop + '.wav'
 pygame.mixer.pre_init()
 # pygame.mixer.init()
 
-pygame.mixer.init(48000, -16, 2, 2048)
+pygame.mixer.init(48000, -16, 2, 1024)
 
 pygame.init()
 
